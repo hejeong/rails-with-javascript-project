96fb42d925628e5130c22ed2d4f23d0190f60b08
hejeong <jonhong777@gmail.com>
2019-05-20 15:23:36 -0400

    Create routes for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "events#index"
+  resources :events
+  resources :users, only: [:new, :create, :index]
+  get '/login', to: 'sessions#new'
+  post '/login/authenticate', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 end
