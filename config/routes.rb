9858d4896c5be7e76a8d0efcd1609d25fe7d80e0
hejeong <jonhong777@gmail.com>
2019-05-22 11:50:11 -0400

    Add additional styling to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
-  resources :events, only: [:create, :index, :show, :destroy]
+  resources :events, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:new, :create, :index]
   get '/login', to: 'sessions#new'
   post '/login/authenticate', to: 'sessions#create'
